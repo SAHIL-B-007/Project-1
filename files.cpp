481101e5d16df83f0b9159b6ef07456babe2c262
SAHIL-B-007 <114509855+SAHIL-B-007@users.noreply.github.com>
2022-10-02 09:44:25 +0530

    Add files via upload

---
+++ files.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+#include <fstream>
+
+using namespace std; 
+ 
+int main ()
+{  
+  ofstream offile();
+  
+  if (filestream.is_open())  
+  {  
+    filestream << "Welcome to javaTpoint.\n";  
+    filestream << "C++ Tutorial.\n";  
+    filestream.close();  
+  }  
+  elsecout <<"File opening is fail.";  
+  return 0;  
+}  
